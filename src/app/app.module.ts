5d7b8abaf8990a082ec1189fc9ef4fcfbe3530f5
Dario Schaffner <darioschaffner7@gmail.com>
2023-05-31 12:59:28 +0200

    add: more custom controls removed: hls player

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { VidstackHlsComponent } from './vidstack-hls/vidstack-hls.component';
-import { VidstackVideoComponent } from './vidstack-video/vidstack-video.component';
 import { VidstackCustomControlsComponent } from './vidstack-custom-controls/vidstack-custom-controls.component';
+import { VidstackVideoComponent } from './vidstack-video/vidstack-video.component';
 import { VistackEventsComponent } from './vistack-events/vistack-events.component';
 
 @NgModule({
-  declarations: [AppComponent, VidstackHlsComponent, VidstackVideoComponent, VidstackCustomControlsComponent, VistackEventsComponent],
+  declarations: [
+    AppComponent,
+    VidstackVideoComponent,
+    VidstackCustomControlsComponent,
+    VistackEventsComponent,
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/vidstack-hls/vidstack-hls.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-vidstack-hls',
-  templateUrl: './vidstack-hls.component.html',
-  styleUrls: ['./vidstack-hls.component.css'],
-})
-export class VidstackHlsComponent {}
